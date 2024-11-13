c4e097649b397bf4696368487093514c00b9cea6
R.J.Ockhuijsen <rens.ockhuijsen@gmail.com>
2024-11-13 17:44:35 +0100

    added jmp and put some codings in to defines

--- include/main.h
+++ include/main.h
@@ -19,15 +19,21 @@
 #define MAX_LABELS 100
 #define LINE_SIZE  50
 
-// Register values
-#define R0 0x00
-#define R1 0x01
-#define R2 0x02
-#define R3 0x03
-#define R4 0x04
-#define R5 0x05
-#define R6 0x06
-#define R7 0x07
+// Register mapping
+#define R0      0x00
+#define R1      0x01
+#define R2      0x02
+#define R3      0x03
+#define R4      0x04
+#define R5      0x05
+#define R6      0x06
+#define R7      0x07
+
+// Instruction mapping
+#define LOAD    0xFF   // not suported int the cpu at the moment
+#define JMP     0x01   // jump based on te lable given
+#define JMPA    0x02   // jump to absolute value
+#define JMPR    0x03   // jump based on register
 
 // Function to convert a register name to its corresponding code
 uint8_t register_to_code(const char *reg);
--- src/Decoder.c
+++ src/Decoder.c
@@ -14,25 +14,29 @@ uint16_t get_instruction_code(const char *instruction, const char *operand1, con
     uint16_t instructionOut;
     uint16_t opcode_code;
     if (strcmp(instruction, "LOAD") == 0) {
-
-        opcode_code = 0001;
-        instructionOut = (atoi(operand2)<< 8) | ((register_to_code(operand1)) << 4) | opcode_code;            // 8 bits for the immediate value (first 8 bits)
+        instructionOut = (atoi(operand2)<< 8) | ((register_to_code(operand1)) << 4) | LOAD;            // 8 bits for the immediate value (first 8 bits)
         
         printf("code %04x\n",instructionOut);
 
         return instructionOut;
     }
-    else if (strcmp(instruction, "ADD") == 0 && strcmp(operand1, "A") == 0 && strcmp(operand2, "B") == 0) {
+    else if (strcmp(instruction, "JMP") == 0) {
+        uint8_t addr = find_label_address(operand1);
+        instructionOut = (addr<< 8) | JMP;
+        return instructionOut;
+    }
+
+
+
+
+    
+    else if (strcmp(instruction, "ADD-example") == 0 && strcmp(operand1, "A") == 0 && strcmp(operand2, "B") == 0) {
         return ADD_A_B;
     }
-    else if (strcmp(instruction, "STORE") == 0 && strcmp(operand1, "A") == 0) {
+    else if (strcmp(instruction, "STORE-example") == 0 && strcmp(operand1, "A") == 0) {
         return STORE_A;
     }
-    else if (strcmp(instruction, "JMP") == 0) {
-        *binary_extra = find_label_address(operand1);
-        if (*binary_extra != -1) return JMP_BASE;
-    }
-    else if (strcmp(instruction, "JEQ") == 0) {
+    else if (strcmp(instruction, "JEQ-example") == 0) {
         *binary_extra = find_label_address(operand1);
         if (*binary_extra != -1) return JEQ_BASE;
     }
