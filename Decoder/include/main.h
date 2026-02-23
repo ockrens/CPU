c57916140d5107b317ef883f3899bf273f8afe1e
ockrens <rens.ockhuijsen@gmail.com>
2026-02-23 22:47:42 +0100

    Made desisons how the program is going to work. might change some of this later on!

---
+++ Decoder/include/main.h
@@ -0,0 +1,34 @@
+/**
+ * @file main.h
+ * @brief header file of the main.cpp file.
+ * 
+ * @author R.J. Ockhuijsen
+ * @date 2026-02-23
+ * @version 0.1
+ * 
+ * @details
+ * 
+ */
+
+
+ /* 
+    these proposed functions are there to make changing how te encoding works easier in the long run.
+
+    to write the right databits to the given adresses.
+    function(start adress, adress count, opcode via struct for control signals).
+
+    defines what cotrol singals need to be set for each instruction.
+    function(opcode, control signals * n). 
+        - control signal examples RD mode, RS mode and functional unit. all ig
+ */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+
+
+#define MemorySize 524288
+
+#endif
--- Decoder/src/main.cpp
+++ Decoder/src/main.cpp
@@ -1,4 +1,39 @@
+/**
+ * @file main.cpp
+ * @brief main file for the decoder program.
+ * 
+ * @author R.J. Ockhuijsen
+ * @date 2026-02-23
+ * @version 0.1
+ * 
+ * @details
+ * 
+ */
+
+#include "../include/main.h"
+
+
+#ifndef MAIN_CPP
+#define MAIN_CPP
+
 using namespace std;
-int main() {
+
+uint8_t rom1Arr[MemorySize]{0};
+uint8_t rom2Arr[MemorySize]{0};
+
+int main(int argc, char const *argv[])  
+{
+    ofstream rom1File("rom1.bin", ios::out | ios::binary);
+    ofstream rom2File("rom2.bin", ios::out | ios::binary);
+
+    if (!rom1File || !rom2File) {
+        cerr << "Failed to open input file(s)\n";
+        return 1;
+    }
+
+    rom1File.close();
+    rom2File.close();
     return 0;
 }
+
+#endif
